706b8afb3b46756e20e61a9d1d181bb26bbfbd40
rhedwan <adesolaridwan2003@gmail.com>
2021-08-18 14:25:51 +0100

    Teststing the 'Server Errror'.

--- core/settings.py
+++ core/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%v=y31#_$@l2259_dficosg40sa$dafdi=%s@jsm7ms_wqf3y9'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
